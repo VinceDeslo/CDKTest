9653602a27fdae7957f89dcb9e5b9d8458d37bc7
VinceDeslo <vincent.desloover.dev@gmail.com>
2022-01-06 19:59:03 -0500

    Initial setup

---
+++ lib/utils/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
+}
